63412af6e748c69df9e60077ab07cfd60b894408
SnezhaKoleva <snezha_koleva@yahoo.com>
2022-01-28 18:23:24 +0200

    Bring back comments

--- src/main/java/qa/automation/MyFirstJava.java
+++ src/main/java/qa/automation/MyFirstJava.java
@@ -1,4 +1,21 @@
 package qa.automation;
 
+
+import java.util.Scanner;
+
 public class MyFirstJava {
-}
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your name");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+        System.out.println("Please enter your hobby");
+        String hobby = scanner.next();
+
+        System.out.println("Hi " + name + "\n I think" +  " " + hobby +" is a nice hobby " );
+
+        }
+    }
+
