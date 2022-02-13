3e10fe398bcb61cbaba482ebdccc237aacdf8d1d
SnezhaKoleva <snezha_koleva@yahoo.com>
2022-02-13 20:46:05 +0200

    Home Work

--- src/main/java/opp/HomeWork/Test.java
+++ src/main/java/opp/HomeWork/Test.java
@@ -8,11 +8,9 @@ public class Test {
 
 
 
-      // Owner ani = new Owner("Ani");
+
 
         Cat murfy = new Cat( "Murfy",new Owner("Ani"),2, true);
-
-      //  murfy.setOwner(ani);
 
         Cat boby = new Cat( "Boby",new Owner("Reni"),5,false );
 
