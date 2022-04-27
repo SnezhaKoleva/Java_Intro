1c2c692088b84f4e397f6b27a5219480eb4eb7a4
SnezhaKoleva <snezha_koleva@yahoo.com>
2022-04-27 08:40:25 +0300

    Home Work 1

--- src/main/java/qa/automation/MyFirstJava.java
+++ src/main/java/qa/automation/MyFirstJava.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class MyFirstJava {
     public static void main(String[] args) {
-
         System.out.println("Please enter your name");
 
         Scanner scanner = new Scanner(System.in);
@@ -14,7 +13,6 @@ public class MyFirstJava {
         String hobby = scanner.nextLine();
 
         System.out.println("Hi " + name + "\n I think" +  " " + hobby +" is a nice hobby " );
-
-        }
     }
+}
 
